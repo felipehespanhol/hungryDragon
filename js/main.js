eab5c8fdfe8adef771f5c9cc1c1ccead927df8e4
Felipe G. Hespanhol <felipeghespanhol@gmail.com>
2017-03-30 19:18:49 -0300

    added instructions

--- js/main.js
+++ js/main.js
@@ -26,6 +26,7 @@ window.onload = function () {
   game.state.add("StateMain",StateMain);
   game.state.add("StateTitle", StateTitle);
   game.state.add("StateOver", StateOver);
-  game.state.start("StateTitle");
+  game.state.add("StateInstructions", StateInstructions);
+  game.state.start("StateInstructions");
   //game.state.start("StateMain");
 };
